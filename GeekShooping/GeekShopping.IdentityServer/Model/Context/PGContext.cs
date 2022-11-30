92c4ae6dbf03cd19837f0123b84dbf4fee4cd77d
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-11-29 22:40:02 -0300

    Implementado o Identity Server

---
+++ GeekShooping/GeekShopping.IdentityServer/Model/Context/PGContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context
+{
+    public class PGContext : IdentityDbContext<ApplicationUser>
+    {
+        public PGContext()
+        {
+
+        }
+        public PGContext(DbContextOptions<PGContext> options)
+            : base(options) { }
+    }
+}
