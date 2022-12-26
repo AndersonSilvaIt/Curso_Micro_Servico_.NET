4c1761e4b1c22ac37b5dbca948173e223aed9b41
anderson.msilva <anderson.msilva@terceiroslinx.com.br>
2022-12-26 17:42:29 -0300

    Implementado o Micro Serviço de E-mail

---
+++ GeekShooping/GeekShopping.Email/Program.cs
@@ -0,0 +1,103 @@
+using GeekShopping.Email.MessageConsumer;
+using GeekShopping.Email.Model.Context;
+using GeekShopping.Email.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+{
+    options.UseNpgsql(connection, builder => builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)
+    .CommandTimeout(10));
+});
+
+var builderopt = new DbContextOptionsBuilder<MySQLContext>();
+
+var provider = builder.Services.BuildServiceProvider();
+var dbContext = provider.GetRequiredService<MySQLContext>();
+
+builder.Services.AddSingleton(new EmailRepository(dbContext));
+
+
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.OrderAPI", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In= ParameterLocation.Header
+                        },
+                        new List<string> ()
+                    }
+                });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeekShopping.OrderAPI v1"));
+}
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+app.Run();
---
+++ GeekShooping/GeekShopping.Email/Repository/EmailRepository.cs
@@ -0,0 +1,29 @@
+﻿using GeekShopping.Email.Messages;
+using GeekShopping.Email.Model;
+using GeekShopping.Email.Model.Context;
+
+namespace GeekShopping.Email.Repository
+{
+    public class EmailRepository : IEmailRepository
+    {
+        private readonly MySQLContext _context;
+
+        public EmailRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public async Task LogEmail(UpdatePaymentResultMessage message)
+        {
+            EmailLog email = new EmailLog()
+            {
+                Email = message.Email,
+                SentDate = DateTime.Now,
+                Log = $"Order - {message.OrderId} has been created successfully!"
+            };
+            //await using var _db = new MySQLContext(_context);
+            _context.Emails.Add(email);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
